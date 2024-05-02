05c5c3ab1509291061a241e0be26bde167b71457
arsalan507 <arsalanahmed507@gmail.com>
2024-05-02 08:04:21 +0300

    added register and login page

---
+++ airbnb-clone/src/pages/LoginPage.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+export default function LoginPage() {
+  return (
+    <div className="mt-4 grow flex items-center justify-around">
+      <div className="mb-64">
+        <h1 className="text-4xl text-center mb-4">Login</h1>
+        <form>
+          <input type="email" placeholder="YourName@email.com" />
+          <input type="password" placeholder="password" />
+          <button className="primary">Login</button>
+          <div className="text-center py-2 text-gray-500">
+            Dont have an account yet?
+            <Link className="underline text-black" to={"/register"}>
+              Register
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
